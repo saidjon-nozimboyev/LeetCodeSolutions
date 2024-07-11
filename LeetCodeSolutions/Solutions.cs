6cfaa4c4329d2327c2823c1d0bddd3c5acfad498
Saidjon <saidjonnozimboyevv@gmail.com>
2024-07-11 20:14:21 +0500

    one solution added

--- LeetCodeSolutions/Solutions.cs
+++ LeetCodeSolutions/Solutions.cs
@@ -1,47 +1,4 @@
-﻿#region TwoSum
-//public class Solution
-//{
-//    public int[] TwoSum(int[] nums, int target)
-//    {
-//        int[] answer = new int[2];
-//        int sub = target - nums[0]; // 6 - 3
-//        int not_take_index = 0; // 0 not taking
-
-
-//        // finding not needed index
-//        for (int i = 0; i < nums.Length - 1; i++)
-//        {
-//            if (target - nums[i] == sub)
-//            {
-//                not_take_index = i;
-//            }
-//        }
-
-//        for (int i = 0; i < nums.Length - 1; i++)
-//        {
-//            if (i == not_take_index)
-//                continue;
-//            else if (nums[i] + sub == target)
-//            {
-//                answer[0] = i;
-//                answer[1] = not_take_index;
-//            }
-//        }
-//        return answer;
-//    }
-//}
-
-//public class Program
-//{
-//    static void Main()
-//    {
-//        Solution solution = new Solution();
-//        Console.WriteLine(string.Join(" ,", solution.TwoSum([3, 2, 4], 6)));
-//    }
-//}
-#endregion
-
-#region FindNonMinOrMax
+﻿#region FindNonMinOrMax
 //public class Solution
 //{
 //    public int FindNonMinOrMax(int[] nums)
@@ -69,19 +26,14 @@ public class Solution
 {
     public int ThirdMax(int[] nums)
     {
-        var distinct_numbers = nums.Distinct();
+        var distinct_numbers = nums.ToHashSet();
         if (distinct_numbers.Count() < 3)
             return distinct_numbers.Max(); 
 
-        var answer = distinct_numbers.ToArray();
-        Array.Sort(answer);
-        var first_max = answer[^1];
-        var second_max = answer[^2];
-        Array.Reverse(answer);
+        var list = distinct_numbers.ToArray();
+        Array.Sort(list);
+        return list[^3];
 
-        int[] newArray = answer.Where((val, index) => val != first_max || val != second_max).ToArray();
-        Array.Sort(newArray);
-        return newArray[^1];
     }
 }
 
---
+++ LeetCodeSolutions/TwoSum.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeSolutions;
+
+public class TwoSumSolution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] answer = new int[2];
+        int sub = target - nums[0]; // 6 - 3
+        int not_take_index = 0; // 0 not taking
+
+
+        // finding not needed index
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (target - nums[i] == sub)
+            {
+                not_take_index = i;
+            }
+        }
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (i == not_take_index)
+                continue;
+            else if (nums[i] + sub == target)
+            {
+                answer[0] = i;
+                answer[1] = not_take_index;
+            }
+        }
+        return answer;
+    }
+    private class Program
+    {
+        static void MainTwoSum()
+        {
+            TwoSumSolution solution = new();
+            Console.WriteLine(string.Join(" ,", solution.TwoSum([3, 2, 4], 6)));
+        }
+    }
+}
+
