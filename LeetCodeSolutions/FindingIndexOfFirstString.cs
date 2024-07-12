2d9e0a193304db9d5130a89c9b51563677453379
Saidjon <saidjonnozimboyevv@gmail.com>
2024-07-12 20:11:42 +0500

    one solution added

---
+++ LeetCodeSolutions/FindingIndexOfFirstString.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeSolutions;
+
+public class FindingIndexOfFirstString
+{
+    public int StrStr(string haystack, string needle)
+    {
+        int index = -1;
+        index = haystack.IndexOf(needle);
+        return index;
+    }
+}
