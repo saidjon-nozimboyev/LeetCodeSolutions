c50c3a47324be7e822662d957b86b4f15cdf3ded
Saidjon <saidjonnozimboyevv@gmail.com>
2024-07-16 22:49:32 +0500

    one solution added

---
+++ LeetCodeSolutions/PowerOfTwo.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCodeSolutions;
+
+internal class PowerOfTwo
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        var ans = Math.Log2 (n);
+        int check = (int)ans;
+        if (ans == check)
+            return true;
+        return false;
+    }
+}
