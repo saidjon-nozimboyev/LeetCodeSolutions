be307fa47754a99c26622398eda546c882ccdc60
Saidjon <saidjonnozimboyevv@gmail.com>
2024-07-17 22:39:41 +0500

    one solution added

---
+++ LeetCodeSolutions/AddDigits.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCodeSolutions
+{
+    internal class AddDigits
+    {
+        public int AddDigit(int num)
+        {
+            if (num == 0)
+                return 0;
+            return (num - 1) % 9 + 1;
+        }
+    }
+}
--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main()
     {
-        FizBuz fizBuz = new FizBuz();
-        Console.WriteLine(string.Join(", ",fizBuz.FizzBuzz(3)));
+        AddDigits addDigits = new AddDigits();
+        Console.WriteLine(addDigits.AddDigit(38));
     }
 }
