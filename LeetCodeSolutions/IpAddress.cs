9f8fa77929bb772a6b17b879ce641a79921e1573
Saidjon <saidjonnozimboyevv@gmail.com>
2024-08-14 19:29:03 +0500

    one solution added

---
+++ LeetCodeSolutions/IpAddress.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    internal class IpAddress
+    {
+        public string DefangIPaddr(string address)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < address.Length; i++)
+            {
+                if (address[i] == '.')
+                {
+                    result.Append("[.]");
+                }
+                else
+                {
+                    result.Append(address[i]);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
